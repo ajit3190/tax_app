cb5a2f93118502e9a7a3060f6369dd81a054cc8f
ajit shetty <shettyajit9@gmail.com>
2018-08-13 00:15:06 +0530

    [Update] ui fixes

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+	def display_tax(item)
+    tax =	item.item_tax
+    if tax.tax_type == "percentage"
+    	postfix = " %"
+    else
+    	postfix = " Rs."
+    end
+    return (tax.tax.to_s + postfix)
+	end
 end
